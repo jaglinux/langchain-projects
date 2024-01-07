98a40614b5177a6dd27327bc2488e0e60b1a9a71
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2024-01-07 01:30:29 -0800

    1/7: Add Run container
    Signed-off-by: Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>

--- docker_wrapper.py
+++ docker_wrapper.py
@@ -14,3 +14,12 @@ print("Number of containers running in this node are ", len(containers))
 print("Containers are ")
 for i, container in enumerate(containers):
     print(i, container, container.attrs['Config']['Image'])
+
+def run_container(image="ubuntu", command="echo hello", detach=False):
+    if detach is False:
+        out = client.containers.run(image, command)
+        print(out)
+
+if __name__ == "__main__":
+    print("enter main")
+    #run_container("jaglinuxdocker/ubuntu-plus:latest", command="lspci -d 1002:")
