184520a00d3466012584f8bec574b7a0ac2410ab
Jagadish Krishnamoorthy <jagdish.krishna@gmail.com>
2024-01-19 12:00:53 -0800

    Create hello.c

---
+++ OpenMP/hello.c
@@ -0,0 +1,11 @@
+#include <omp.h>
+#include <cstdio>
+
+int main() {
+    #pragma omp parallel
+    {
+        int ID=omp_get_thread_num();
+        printf("Hello %d", ID);
+        printf("World %d \n", ID);
+    }
+}
